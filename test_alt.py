8cc4582a817844190ded3558a53336556d753fff
nutti <nutti.metro@gmail.com>
2017-02-26 16:31:24 +0900

    Test add-on with Travis CI

--- test_alt.py
+++ test_alt.py
@@ -10,9 +10,9 @@ if __name__ == "__main__":
         assert result == {'FINISHED'}, "test_ops_1にエラーが存在します"
         # bl_name = object.test_ops_2 のテスト
         assert bpy.ops.object.test_ops_2 != None, "test_ops_2が有効化されていません"
-        bpy.ops.object.select_all(action='DESELECT')    # オブジェクトの選択を解除
-        bpy.data.objects['Cube'].select = True          # オブジェクト名が「Cube」のオブジェクトを削除
-        bpy.ops.object.delete()                         # 選択中のオブジェクトを削除
+        bpy.ops.object.select_all(action='DESELECT')    # $オブジェクトの選択を解除
+        bpy.data.objects['Cube'].select = True          # $オブジェクト名が「Cube」のオブジェクトを削除
+        bpy.ops.object.delete()                         # $選択中のオブジェクトを削除
         result = bpy.ops.object.test_ops_2()
         assert result == {'FINISHED'}, "test_ops_2にエラーが存在します"       # オブジェクト「Cube」は削除済みのためエラーとなる
     # テスト失敗時の処理
